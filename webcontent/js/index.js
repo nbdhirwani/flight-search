fcda2e833246a7732658ccd73ffb30774aea46fb
Nirav Dhirwani <niravdhirwani@nbnco.com.au>
2016-09-22 16:40:18 +1000

    Client Complete - Without styling

--- webcontent/js/index.js
+++ webcontent/js/index.js
@@ -1,12 +1,31 @@
-var rootURL = 'http://localhost:3000';
+// var rootURL = "http://localhost:3000";
+var rootURL = "http://node.locomote.com/code-task";
+// Set up airline selection
+$(document).ready(function() {
+  var airlinesURL = rootURL + "/airlines";
+  $.ajax({
+      type: "GET",
+      url: airlinesURL,
+      dataType: "json", // data type of response
+      success: populateAirlines
+  });
+});
+
+function populateAirlines(data) {
+  var airlineSelection = $("#airline-sel");
+  airlineSelection.find("option").remove();
+  $.each(data, function(key, value){
+    airlineSelection.append("<option value=" + value.code + ">" + value.name + " [" + value.code + "]" + "</option>");
+  });
+}
 
 // Set up from-txt autocomplete
 var optionsFrom = {
   url: function(phrase) {
-    return rootURL + '/airports?q=' + phrase;
+    return rootURL + "/airports?q=" + phrase;
   },
   getValue: function(element){
-    return element.airportName + ', ' + element.cityName + ' [' + element.airportCode + ']';
+    return element.airportName + ", " + element.cityName + " [" + element.airportCode + "]";
   },
   list: {
     onSelectItemEvent: function() {
@@ -15,15 +34,15 @@ var optionsFrom = {
     }
   }
 };
-$('#from-txt').easyAutocomplete(optionsFrom);
+$("#from-txt").easyAutocomplete(optionsFrom);
 
 // Set up to-txt autocomplete
 var optionsTo = {
   url: function(phrase) {
-    return rootURL + '/airports?q=' + phrase;
+    return rootURL + "/airports?q=" + phrase;
   },
   getValue: function(element){
-    return element.airportName + ', ' + element.cityName + ' [' + element.airportCode + ']';
+    return element.airportName + ", " + element.cityName + " [" + element.airportCode + "]";
   },
   list: {
     onSelectItemEvent: function() {
@@ -32,22 +51,24 @@ var optionsTo = {
     }
   }
 };
-$('#to-txt').easyAutocomplete(optionsTo);
+$("#to-txt").easyAutocomplete(optionsTo);
 
 
 // Set up button click event
-$('#search-btn').on('click', function(){
+$("#search-btn").on("click", function(e){
+  e.preventDefault();
   findFlights();
 })
 
 function findFlights(){
-  var travelDate = $('#travel-date-txt').val();
-  var fromPort = $('#from-hdn').val();
-  var toPort = $('#to-hdn').val();
-  var flightSearchURL = rootURL + '/search?date=' + travelDate + '&from=' + fromPort + '&to=' + toPort;
+  var travelDate = $("#travel-date-txt").val();
+  var fromPort = $("#from-hdn").val();
+  var toPort = $("#to-hdn").val();
+  var airline = $("#airline-sel").val();
+  var flightSearchURL = rootURL + "/flight_search/"+ airline + "?date=" + travelDate + "&from=" + fromPort + "&to=" + toPort;
 
   $.ajax({
-      type: 'GET',
+      type: "GET",
       url: flightSearchURL,
       dataType: "json", // data type of response
       success: renderFlightInformation
@@ -55,5 +76,41 @@ function findFlights(){
 }
 
 function renderFlightInformation(data){
-  console.log('Airline Name: ' + data[0].airline.name);
+
+  var dataSet = [];
+  for(var index in data) {
+    dataSet.push([
+      data[index].airline.name,
+      data[index].start.dateTime,
+      data[index].start.airportName,
+      data[index].start.cityName,
+      data[index].start.countryName,
+      data[index].finish.dateTime,
+      data[index].finish.airportName,
+      data[index].finish.cityName,
+      data[index].finish.countryName,
+      data[index].plane.shortName,
+      "$" + data[index].price
+    ]);
+  }
+  console.log(JSON.stringify(dataSet));
+  $('#result').DataTable({
+    destroy: true,
+    searching: false,
+    data: dataSet,
+    order: [[ 10, "asc" ]],
+    columns: [
+        { title: "Airline Name" },
+        { title: "Departure Date" },
+        { title: "Departure Airport" },
+        { title: "Departure City" },
+        { title: "Departure Country" },
+        { title: "Arrival Date" },
+        { title: "Arrival Airport" },
+        { title: "Arrival City" },
+        { title: "Arrival Country" },
+        { title: "Aircraft" },
+        { title: "Price" }
+      ]
+    });
 }
